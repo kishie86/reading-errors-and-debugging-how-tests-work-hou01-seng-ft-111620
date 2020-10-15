f7048735aa58e37113cd17b1f7724389ec6d2c47
Kishana Liburd <kishana.liburd@gmail.com>
2020-10-15 21:08:29 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,6 @@ second_number = 2
 sum = 3
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
   
   # Add your variables here
